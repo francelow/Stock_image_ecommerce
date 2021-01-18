d6e8203469bb2b9813cafe095f89d2566c54a19e
Francois Low <45155013+francelow@users.noreply.github.com>
2021-01-18 11:03:58 +0800

    Add files via upload

---
+++ resources/templates/back/add_product.php
@@ -0,0 +1,40 @@
+<div class="col-md-12">
+
+<div class="row">
+<h1 class="page-header">
+   Add Product
+
+</h1>
+</div>
+
+
+
+<form action="" method="post" enctype="multipart/form-data">
+
+
+<div class="col-md-8">
+
+<div class="form-group">
+    <label for="product-title">Product Title </label>
+        <input type="text" name="product_title" class="form-control">
+
+    </div>
+
+
+    <div class="form-group">
+           <label for="product-title">Product Description</label>
+      <textarea name="product_description" id="" cols="30" rows="10" class="form-control"></textarea>
+    </div>
+
+
+
+    <div class="form-group row">
+
+      <div class="col-xs-3">
+        <label for="product-price">Product Price</label>
+        <input type="number" name="product_price" class="form-control" size="60">
+      </div>
+    </div>
+
+
+</div><!--Main Content-->
